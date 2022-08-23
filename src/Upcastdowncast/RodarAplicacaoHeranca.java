87f769e3279f83cfe8683e2d4e4034f7b0fecda5
filipesenaifirjan <filipemattossenai@gmail.com>
2022-08-23 07:33:05 -0300

    sobrescrita

--- src/Upcastdowncast/RodarAplicacaoHeranca.java
+++ src/Upcastdowncast/RodarAplicacaoHeranca.java
@@ -4,14 +4,18 @@ public class RodarAplicacaoHeranca {
 	
 	public static void main(String[] args) {
 		
+		@SuppressWarnings("unused")
 		Funcionario funcionario = new Funcionario();
 		
+				@SuppressWarnings("unused")
 				Funcionario gerente = new Gerente();
+				@SuppressWarnings("unused")
 				Funcionario vendedor = new Vendedor();
+				@SuppressWarnings("unused")
 				Funcionario faxineiro = new Faxineiro();
 				
 				//Gerente gerente_ = new Funcionario();
-				Vendedor vendedor_ = (Vendedor) new Funcionario();
+				//Vendedor vendedor_ = (Vendedor) new Funcionario();
 	}
 
 }
