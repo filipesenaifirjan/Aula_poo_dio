9f2ed730dbc8683a1a8c986ac98b66bf923187e4
filipesenaifirjan <filipemattossenai@gmail.com>
2022-08-23 10:26:38 -0300

    associação-dependencia e agregação

--- src/AssociacaoExemplos/Disciplina.java
+++ src/AssociacaoExemplos/Disciplina.java
@@ -1,5 +1,6 @@
 package AssociacaoExemplos;
 
 public class Disciplina {
-
+Aluno aluno;
 }
+//agregação, por exemplo, se a disciplina deixar de existir, não necessariamente o aluno deixa de existir
