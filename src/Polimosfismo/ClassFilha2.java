60c9f53b52cc4d5f0ca857b26e25004537ed8523
filipesenaifirjan <filipemattossenai@gmail.com>
2022-08-23 08:10:51 -0300

    polimorfismo

---
+++ src/Polimosfismo/ClassFilha2.java
@@ -0,0 +1,14 @@
+package Polimosfismo;
+
+public class ClassFilha2 extends ClassMae {
+	
+	@Override
+	void metodo1() {
+		System.out.println("Medoto 1 da classe filha 2");
+	}
+	
+	@Override
+	void metodo2() {
+		System.out.println("Medoto 2 da classe filha 2");
+	}
+}
---
+++ src/Polimosfismo/ClassMae.java
@@ -0,0 +1,5 @@
+package Polimosfismo;
+
+public class ClassMae {
+
+}
