2e232749f1a6f1b06149ccab2830dddf492cc988
filipesenaifirjan <filipemattossenai@gmail.com>
2022-08-29 10:48:21 -0300

    interface

--- src/OperacaoMatematica.java
+++ src/OperacaoMatematica.java
@@ -2,8 +2,11 @@
 public interface OperacaoMatematica {
 
 	void soma(double operando1, double operando2);
+	
 	void subtracao(double operando1, double operando2);
+	
 	void multiplicando(double operando1, double operando2);
+	
 	void divisao(double operando1, double operando2);
 	
 }
